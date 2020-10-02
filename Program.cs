a57ba6314a773bcf96f1b864b751a0af2d91d663
Glenn Watson <GitHub@glennwatson.net>
2020-10-02 17:32:58 +1000

    Update website to use Statiq

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace GlennWatson.Website
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-AU");
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToNetlify(
+                    Config.FromSetting<string>("NETLIFY_SITE_ID"),
+                    Config.FromSetting<string>("NETLIFY_DEPLOY_KEY"))
+                .RunAsync();
+        }
+    }
+}
